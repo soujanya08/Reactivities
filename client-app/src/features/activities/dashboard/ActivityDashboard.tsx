a961fffa7828eb21681b726e0269b91b248593dd
soujanya08 <saikiranmandula1@gmail.com>
2021-08-08 17:51:37 +0530

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
+import React, { useEffect } from 'react'
+
 import ActivityList from './ActivityList';
 import {Grid} from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 
 
 export default observer(function ActivityDashboard() {
   const {activityStore}=useStore();
-  const{selectedActivity,editMode}=activityStore;
+  const {loadActivities,activityRegistry}=activityStore;
+ 
+
+
+  useEffect(() => {
+    if(activityRegistry.size<=1) loadActivities();
+  }, [activityRegistry.size,loadActivities])
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
+
     return (
       <Grid>
         <Grid.Column width="10">
@@ -19,14 +28,7 @@ export default observer(function ActivityDashboard() {
                 />
         </Grid.Column>
         <Grid.Column width='6'>
-          {selectedActivity && !editMode &&
-          <ActivityDetails
-  
-          />}
-           {editMode &&
-          <ActivityForm 
-          
-        />}
+        <h2>Activity Filter</h2>
         </Grid.Column>
       </Grid>
     );
